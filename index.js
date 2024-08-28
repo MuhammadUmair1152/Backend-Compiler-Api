8cd11eae2f1ae57434456defc90c3ca497347f20
Muhammad Umair <muhammadumair1152@gmail.com>
2024-08-28 20:49:42 +0500

    changes

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use('/api/code', codeRoutes);
 app.use(errorHandler);   // Error handling middleware
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
